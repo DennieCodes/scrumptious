f5cdba710688742d6ad0795f28318e5864fb03f2
Dennie <oculareo@gmail.com>
2023-08-24 15:30:27 -0400

    Added update views and html

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from recipes.views import show_recipe, recipe_list, create_recipe
+from recipes.views import show_recipe, recipe_list, create_recipe, update_recipe
 
 urlpatterns = [
     path("recipes/create/", create_recipe, name="create_recipe"),
     path("recipes/", recipe_list, name="recipe_list"),
     path("recipes/<int:id>/", show_recipe, name="show-recipe"),
+    path("recipes/<int:id>/update", update_recipe, name="update_recipe")
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render, get_object_or_404, redirect
 from recipes.models import Recipe
 from recipes.forms import RecipeForm
 
+# UPDATE RECIPE
+def update_recipe(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('show-recipe', id=id)
+    else:
+        form = RecipeForm(instance=recipe)
+
+    context = {
+        'recipe': recipe,
+        'form': form
+    }
+    return render(request, "recipes/update.html", context)
+
+# CREATE RECIPE
 def create_recipe(request):
     if request.method == 'POST':
         form = RecipeForm(request.POST)
@@ -17,6 +36,7 @@ def create_recipe(request):
 
     return render(request, 'recipes/create.html', context)
 
+# SHOW RECIPE
 def show_recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id)
     context = {
@@ -24,6 +44,7 @@ def show_recipe(request, id):
     }
     return render(request, "recipes/detail.html", context)
 
+# SHOW ALL RECIPES
 def recipe_list(request):
     recipes = Recipe.objects.all()
     context = {
@@ -31,10 +52,7 @@ def recipe_list(request):
     }
     return render(request, "recipes/list.html", context)
 
-def update_recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
-    pass
-
+# DELETE RECIPE
 def delete_recipe(request, id):
-    recipe = Recipe.objects.get(id=id)
-    pass
+    if request.method == 'DELETE':
+        pass
